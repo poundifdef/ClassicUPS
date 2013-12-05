45990590d79e374d8e182f4e38091fe9e1734e56
Jay Goel <jay@classicspecs.com>
2013-12-05 17:32:23 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ClassicUPS',
-    version='0.1.5',
+    version='0.1.6',
     author='Jay Goel',
     author_email='jay@classicspecs.com',
     url='http://github.com/classicspecs/ClassicUPS/',
