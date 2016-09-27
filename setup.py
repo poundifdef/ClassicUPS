06be6ec2d0bf80a5b2feffc4c684863bc56326ab
Ismael Calvo <ismael.calvo@factorlibre.com>
2016-09-27 13:19:23 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ClassicUPS',
-    version='0.2.0',
+    version='0.2.1',
     author='Jay Goel',
     author_email='jay@classicspecs.com',
     url='http://github.com/classicspecs/ClassicUPS/',
